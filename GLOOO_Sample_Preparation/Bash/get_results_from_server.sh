2386dc22644fcd20dd3fa898ba89bcbb63b3f637
anwaldt <voncoler@tu-berlin.de>
2016-08-23 13:27:56 +0200

    Added shell scripts ...

---
+++ GLOOO_Sample_Preparation/Bash/get_results_from_server.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+#
+#
+#
+scp -r audiolab@audiolab:~/Projects/sample_preparation/Sinusoidal_Data_TXT /media/henrikvoncoler/HVC/GLOOO_Sample_Preparation/
+#
+#
+scp -r audiolab@audiolab:~/Projects/sample_preparation/Sinusoidal_Data /media/henrikvoncoler/HVC/GLOOO_Sample_Preparation/
+#
+#
+scp -r audiolab@audiolab:~/Projects/sample_preparation/Residual /media/henrikvoncoler/HVC/GLOOO_Sample_Preparation/
+#
+#
+scp -r audiolab@audiolab:~/Projects/sample_preparation/Tonal /media/henrikvoncoler/HVC/GLOOO_Sample_Preparation/
+#
+#
+scp -r audiolab@audiolab:~/Projects/sample_preparation/Complete /media/henrikvoncoler/HVC/GLOOO_Sample_Preparation/
---
+++ GLOOO_Sample_Preparation/Bash/send_audio_to_server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+#
+scp *.wav audiolab@audiolab:~/Projects/sample_preparation/Samples
