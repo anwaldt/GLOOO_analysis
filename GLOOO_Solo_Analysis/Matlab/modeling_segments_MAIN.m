21243046ffc1e087f5e153cefc764d60fe08fc88
henrikvoncoler <voncoler@tu-berlin.de>
2019-01-22 17:21:30 +0100

    Calculated new set with 80 partials - leaving Wintermute!

--- GLOOO_Solo_Analysis/Matlab/modeling_segments_MAIN.m
+++ GLOOO_Solo_Analysis/Matlab/modeling_segments_MAIN.m
@@ -25,20 +25,20 @@ restoredefaultpath
 
 
 % Set the (output) path for this set
-ds = datestr(now,'yyyy-mm-dd'); 
-% ds = '2019-01-17';
+% ds = datestr(now,'yyyy-mm-dd'); 
+ds = '2019-01-21';
 
 % set this false for debugging
 % (enables breakpoints in parfor loops)
 run_parallel             = 1;
 
 % overwrite existing results
-renew_all                = 1;
+renew_all                = 0;
 
 % Decide which parts should be executed:
-do_basic_analysis        = 1;
-do_partial_analysis      = 1;
-do_modeling_segments     = 1;
+do_basic_analysis        = 0;
+do_partial_analysis      = 0;
+do_modeling_segments     = 0;
 
 % only for single sounds:
 do_statistical_sms       = 1;
@@ -61,12 +61,12 @@ micToDo     = 'BuK';
 % chose whether to process all files,
 % a single file by name, or a subset:
 
-% filesToDo  = 'All';
+filesToDo  = 'All';
 
 % filesToDo  = '1-oct-sweep.wav';
 % filesToDo  = 'TwoNote_BuK_22.wav';
 
-filesToDo  = 'SampLib_BuK_136.wav';
+%filesToDo  = 'SampLib_BuK_136.wav';
  %filesToDo   = 'TwoNote_BuK_22.wav';
 % filesToDo   = 'SampLib_BuK_301.wav';
 % filesToDo   = 'SampLib_BuK_332.wav';
@@ -163,8 +163,8 @@ end
 
 if do_basic_analysis == true
     
-    %parfor (fileCNT = filesToDo,parMode)
-     for fileCNT = filesToDo
+    parfor (fileCNT = filesToDo,parMode)
+     %for fileCNT = filesToDo
         
         
         
@@ -186,7 +186,7 @@ end
 if do_partial_analysis == true
     
     %parfor (fileCNT = filesToDo,parMode)
-    for fileCNT = filesToDo        
+     for fileCNT = filesToDo        
         
         if param.info == true
             disp(['starting partial analysis for: ',fileNames{fileCNT}]);
@@ -211,8 +211,8 @@ end
 
 if do_modeling_segments == true
     
-    % parfor (fileCNT = filesToDo,parMode)
-      for  fileCNT = filesToDo
+       parfor (fileCNT = filesToDo,parMode)
+      %for  fileCNT = filesToDo
         
         if param.info == true
             disp(['starting modeling for: ',fileNames{fileCNT}]);
